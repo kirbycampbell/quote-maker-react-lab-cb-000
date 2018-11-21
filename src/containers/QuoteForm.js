27bce90717332146ff9d0ea8ec3af9149a4dfb53
Kirby Campbell <jkirbycampbell@gmail.com>
2018-11-21 19:18:58 +0000

    automatically backed up by learn

--- src/containers/QuoteForm.js
+++ src/containers/QuoteForm.js
@@ -4,20 +4,24 @@ import { connect } from 'react-redux';
 import { addQuote } from '../actions/quotes';
 
 class QuoteForm extends Component {
-
   state = {
-    //set up a controlled form with internal state
+    content: '',
+    author: ''
   }
-
   handleOnChange = event => {
-    // Handle Updating Component State
+    const { value, name } = event.target;
+    this.setState({
+      [name]: value
+    });
   }
-
   handleOnSubmit = event => {
-    // Handle Form Submit event default
-    // Create quote object from state
-    // Pass quote object to action creator
-    // Update component state to return to default state
+    event.preventDefault();
+    const quote = {...this.state, id: uuid() };
+    this.props.addQuote(quote);
+    this.setState({
+      content: '',
+      author: ''
+    });
   }
 
   render() {
